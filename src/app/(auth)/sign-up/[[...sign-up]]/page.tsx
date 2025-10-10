7d5b496eb02818e8b15fe0aea6a0d9687ea74391
pakmmi <68132541+paulkamani9@users.noreply.github.com>
2025-10-10 15:28:13 +0400

    refactor(auth): remove unused authentication components and pages

--- src/app/(auth)/sign-up/[[...sign-up]]/page.tsx
+++
@@ -1,9 +0,0 @@
-import { SignUp } from "@clerk/nextjs";
-
-export default function SignUpPage() {
-  return (
-    <div className="flex w-full flex-1 items-center justify-center p-6 md:p-10 relative z-10">
-      <SignUp />
-    </div>
-  );
-}
